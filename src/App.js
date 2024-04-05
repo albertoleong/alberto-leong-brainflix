b6bd3cc07da8bdc3362e0599064ab6f44bb62d5c
Alberto Leong <117922475+albertoleong@users.noreply.github.com>
2024-04-04 22:24:28 -0400

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <>
     <Header />
     <Hero />
+    <main className='main'>
+
+    </main>
     </>
   );
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,21 @@
 import "./Hero.scss"
-import video from "../../assets/images/Upload-video-preview.jpg"
+import video from '../../assets/images/Upload-video-preview.jpg'
 import scrub from "../../assets/icons/scrub.svg"
+import play from "../../assets/icons/play.svg"
+import screen from "../../assets/icons/fullscreen.svg"
+import sound from '../../assets/icons/volume_up.svg'
 
 const Hero = () => {
 
     return (
         <div className="hero">
-            <video className="hero__video" poster={video} controls>
+            <video className="hero__video" poster='' controls>
             </video>
-            {/* <img className="hero__scrub" src={scrub} alt="scrub"></img> */}
+            {/* <img className="hero__play" src={play} alt="play" />
+            <img className="hero__scrub" src={scrub} alt="scrub"></img> 
+            <img className="hero__screen" src={screen} alt="fullscreen" />
+            <img className="hero__sound" src={sound} alt="sound control" /> */}
+            
         </div>
     )
 }
